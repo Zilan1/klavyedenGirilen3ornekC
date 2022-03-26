eeadca33dd6f7e1ead5cdb0d273811a3eb5666da
Zilan1 <79017444+Zilan1@users.noreply.github.com>
2022-03-26 15:29:50 +0300

    sum

---
+++ proje48.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	
+  //*****Klavyeden Girilen Sayini Kupunu Alan Program*****
+
+	//int sayi,sonuc;
+	
+    //printf("Sayiyi Giriniz: ");
+    //scanf("%d",&sayi);
+  
+    //sonuc=sayi*sayi*sayi;
+  
+    //printf("%d",sonuc);  
+  
+  
+ //***** Klavyeden Girilen Sayinin Tam Bolenlerini Bulan Program *****
+  
+   //int i,sayi;
+  
+   //printf("Sayiyi Giriniz: ");
+   //scanf("%d",&sayi);
+
+   //for(i=1;i<=sayi;i++)
+   //{
+   	//if(sayi%i==0)
+   	//{
+   		//printf("%d\n",i);
+	//}
+   //}
+
+ // Uc Basamakli Rakamlari Birbirinden Farkli Kac Tane Sayi Oldugunu Bulan Program
+ 
+	//int sayac=0,i,a,b,c,sayi;
+	
+	//for(i=100;i<=999;i++)
+	//{
+		//a=i/100;
+		//b=(i/10)%10;
+		//c=i%10;
+		//if(a!=b && a!=c && b!=c)
+		//{
+			//sayac++;
+		//}
+	//}
+	
+	//printf("%d",sayac);
+	
+	return 0;
+}
